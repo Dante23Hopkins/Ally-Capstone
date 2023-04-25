6f66cd0831512aef61fc36982fdfe9770e2a8c73
kraggio <keilyraggio18@gmail.com>
2023-04-25 11:53:39 -0400

    fixing routes

--- allycapstone/src/App.js
+++ allycapstone/src/App.js
@@ -1,21 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Volunteer from "./pages/Volunteer";
+
 import "./App.css";
-export default function App() {
+export const AppContext = React.createContext();
+function App() {
+  const [basename, setBasename] = useState("/");
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
+    <AppContext.Provider value={{ basename, setBasename }}>
+      <Router basename={basename}>
+        <Navbar />
+        <Routes path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="volunteer" element={<Volunteer Hours/>} />
+          <Route path="volunteer" element={<Volunteer Hours />} />
           <Route path="login" element={<Login />} />
           <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </Router>
+    </AppContext.Provider>
   );
 }
+
+export default App;
